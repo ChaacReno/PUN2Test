64102233043553d0eaf2ee2313077f3eaf8f1abb
Sébastien <sebastien.levesque.slev@gmail.com>
2023-08-29 16:58:14 -0400

    feat: add icon add logic

---
+++ Assets/Scripts/Net_RoomPlayerListHandler.cs
@@ -0,0 +1,27 @@
+using Photon.Pun;
+using UnityEngine;
+
+public class Net_RoomPlayerListHandler : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private GameObject Icon;
+    [SerializeField] private Transform IconLocation;
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        PrintIcons();
+    }
+
+    private void PrintIcons()
+    {
+        var playerCount = PhotonNetwork.CurrentRoom.PlayerCount;
+        var iconCount = IconLocation.childCount;
+
+        var countToInstantiate = playerCount - iconCount;
+
+        for (int i = 0; i < countToInstantiate; i++)
+        {
+            Instantiate(Icon, IconLocation);
+        }
+    }
+}
